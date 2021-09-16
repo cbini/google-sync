7cbaa192a313b59670eb0a353348a51789098c49
Charlie Bini <cbini87@gmail.com>
2021-09-16 12:22:12 -0500

    add region to printf

--- sync-google.sh
+++ sync-google.sh
@@ -37,10 +37,10 @@ do
     mkdir -p $PROJECT_DIR/data/$region
     mkdir -p $PROJECT_DIR/log/$region
 
-    printf "Creating users..."
+    printf "$region - Creating users...\n"
     create_file=$dir/user_create.csv
     if [ -f $create_file ]; then
-        printf "\n$create_file\n"
+        printf "$create_file\n"
 
         filename=$(basename -- "$create_file")
         filename="${filename%.*}"
@@ -57,11 +57,11 @@ do
 
         rm $create_file
     else
-        printf "\nNo users to create!"
+        printf "No users to create!\n"
     fi
-    printf "\n\n"
+    printf "\n"
 
-    printf "Syncing user group membership...\n"
+    printf "$region - Syncing user group membership...\n"
     group_file=$dir/group.csv
     if [ -f $group_file ]; then
         filename=$(basename -- "$group_file")
@@ -73,9 +73,9 @@ do
             sync member notsuspended nomail \
             ou_and_children "/Students/~~region~~"
     fi
-    printf "\n\n"
+    printf "\n"
 
-    printf "Creating Reset Student PW admins..."
+    printf "$region - Creating Reset Student PW admins...\n"
     admin_file=$dir/admin_create.csv
     if [ -f $admin_file ]; then
         filename=$(basename -- "$admin_file")
@@ -89,9 +89,9 @@ do
 
         rm $admin_file
     else
-        printf "\nNo admins to create!"
+        printf "No admins to create!\n"
     fi
-    printf "\n\n"
+    printf "\n"
 done
 
 for dir in $PROJECT_DIR/data/*/; 
@@ -102,10 +102,10 @@ do
     mkdir -p $PROJECT_DIR/data/$region
     mkdir -p $PROJECT_DIR/log/$region
 
-    printf "Updating users w/ pw..."
+    printf "$region - Updating users w/ pw...\n"
     update_pw_file=$dir/user_update_pw.csv
     if [ -f $update_pw_file ]; then
-        printf "\n$update_pw_file\n"
+        printf "$update_pw_file\n"
         
         filename=$(basename -- "$update_pw_file")
         filename="${filename%.*}"
@@ -122,14 +122,14 @@ do
         
         rm $update_pw_file
     else
-        printf "\nNo users to update w/ pw!"
+        printf "No users to update w/ pw!\n"
     fi
-    printf "\n\n"
+    printf "\n"
 
-    printf "Updating users w/o pw..."
+    printf "$region - Updating users w/o pw...\n"
     update_nopw_file=$dir/user_update_nopw.csv
     if [ -f $update_nopw_file ]; then
-        printf "\n$update_nopw_file\n"
+        printf "$update_nopw_file\n"
         
         filename=$(basename -- "$update_nopw_file")
         filename="${filename%.*}"
@@ -145,7 +145,7 @@ do
         
         rm $update_nopw_file
     else
-        printf "\nNo users to update w/o pw!"
+        printf "No users to update w/o pw!\n"
     fi
-    printf "\n\n"
+    printf "\n"
 done
